9ba2b1e6711db0cd4fefe41595176c56e7b55193
tithi2149 <tanyasultana60@gmail.com>
2023-10-22 12:47:33 +0600

    created and read the data for client side add product page

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USERID}:${process.env.USERPASS}@cluster0.3vwvgpx.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,9 +29,49 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
+        const database = client.db("Products");
+        const ProductsCollection = database.collection("products");
+
+        const database1 = client.db("brands");
+        const brandsCollection = database1.collection("brands");
+
+        app.get("/Products",async(req,res)=>{
+
+          const cursor=ProductsCollection.find()
+          const result = await cursor.toArray()
+          res.send(result)
+
+
+        })
+        app.get("/brands",async(req,res)=>{
+
+          const cursor=brandsCollection.find()
+          const result = await cursor.toArray()
+          res.send(result)
+
+
+        })
+
+        app.post("/brands",async(req,res)=>{
+          const newbrand=req.body
+          const result = await brandsCollection.insertOne(newbrand);
+          res.send(result)
+          console.log(newbrand)
+        })
+
+        app.post("/Products",async(req,res)=>{
+          const newuser=req.body
+          const result = await ProductsCollection.insertOne(newuser);
+          res.send(result)
+          console.log(result)
+        })
+
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
